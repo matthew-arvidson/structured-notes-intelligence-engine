90a6a3637d52935019ae4997dc887c4c93eadff9
Matthew Arvidson <matthew.arvidson@riskcare.com>
2026-07-20 13:41:32 -0400

    fix theme issue

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" data-theme="dark" className="h-full">
       <body className="flex flex-col h-screen min-h-0 bg-[#141516] text-gray-300">
         <main className="flex-1 flex flex-col min-h-0">{children}</main>
         <footer className="flex-shrink-0 bg-[#042341] px-2 py-1 text-xs text-gray-500 text-center">
